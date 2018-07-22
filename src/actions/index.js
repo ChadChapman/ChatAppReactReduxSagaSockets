dc40d7040cee25fd553e54e65a9857cac32b3056
git_dave <ctchapman@gmail.com>
2018-07-22 14:53:38 -0700

    added a reducer just for messages

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import * as types from '../constants/ActionTypes'
 
 let nextMessageId = 0;
-const nextUserId = 0;
+let nextUserId = 0;
 
 export const addMessage = (message, author) => ({
     type: types.ADD_MESSAGE,
@@ -19,6 +19,8 @@ export const addUser = name => ({
 export const messageReceived = (message, author) => ({
     type: types.MESSAGE_RECEIVED,
     id: nextMessageId++,
+    message,
+    author
 });
 
 export const populateUsersList = (users) => ({
