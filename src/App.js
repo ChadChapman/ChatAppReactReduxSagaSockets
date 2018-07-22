b4c10e4e2307cab7b2cc8f9b55efbeb2fb8b94d1
git_dave <ctchapman@gmail.com>
2018-07-22 14:18:32 -0700

    harcoded html sections

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import  { Sidebar } from "./containers/Sidebar"
+import  { MessagesList } from "./containers/MessagesList"
+import  { AddMessage } from "./containers/AddMessage"
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div id="container">
+          <aside id="sidebar">Users</aside>
+            <section id="main">
+                <section id="messages-list">MessagesList</section>
+                <section id="new-message">New Messages</section>
+            </section>
+
       </div>
     );
   }
