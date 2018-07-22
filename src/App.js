1aca069f017df0bb0183e522d86287f0bc6736ba
git_dave <ctchapman@gmail.com>
2018-07-22 14:25:30 -0700

    index.js now uses Provider component from Redux

--- src/App.js
+++ src/App.js
@@ -9,10 +9,10 @@ class App extends Component {
   render() {
     return (
       <div id="container">
-          <aside id="sidebar">Users</aside>
+          <Sidebar />
             <section id="main">
-                <section id="messages-list">MessagesList</section>
-                <section id="new-message">New Messages</section>
+                <MessagesList />
+                <AddMessage />
             </section>
 
       </div>
